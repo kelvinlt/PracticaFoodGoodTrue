10018011d227b53682e0743cd583e406a529f075
kelvinlt <kelvinleethen@gmail.com>
2018-06-13 13:45:41 +0200

    Añadido a EJB funciones del viejo

--- src/java/model/FoodEjb.java
+++ src/java/model/FoodEjb.java
@@ -5,7 +5,12 @@
  */
 package model;
 
+import entities.Dish;
+import entities.User;
+import exceptions.FoodGoodExceptions;
+import java.util.List;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 
@@ -17,5 +22,33 @@ import javax.persistence.PersistenceUnit;
 public class FoodEjb {
 
     @PersistenceUnit EntityManagerFactory emf;
+    public void altaUser(User u) throws FoodGoodExceptions{
+        EntityManager em = emf.createEntityManager();
+        User aux = em.find(User.class, u.getUsername());
+        if(aux !=null){
+            em.close();
+            throw new FoodGoodExceptions("Ya existe un user con el mismo username");
+        }
+        em.persist(u);
+        em.close();
+    }
     
+    public List<User> listadoUser(){
+        return emf.createEntityManager().createNamedQuery("User.findAll").getResultList();
+    }
+    
+    public void altaDish(Dish d) throws FoodGoodExceptions{
+        EntityManager em = emf.createEntityManager();
+        Dish aux = em.find(Dish.class, d.getName());
+        if(aux !=null){
+            em.close();
+            throw new FoodGoodExceptions("Ya existe un plato con el mismo nombre");
+        }
+        em.persist(d);
+        em.close();
+    }
+    
+    public List<Dish> listadoDish(){
+        return emf.createEntityManager().createNamedQuery("Dish.findAll").getResultList();
+    }
 }
